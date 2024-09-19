3435c70eb84a356ffec42dec49cab096b4492474
Hasbullah2004 <muhammadhasbullah2004@gmail.com>
2024-09-19 23:38:00 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+nama = input("Masukkan nama lengkap kamu: ")
+nama_panggilan = input("Masukkan nama panggilan kamu: ")
+print("\nTerima kasih telah memperkenalkan diri!")
+print("Nama lengkap saya adalah:", nama)
+print("Kamu bisa memanggil saya:", nama_panggilan)
