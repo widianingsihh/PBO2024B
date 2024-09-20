05a738a4d45e6a5f35a3f9723a86d0690cbb5292
haniboy87 <154058137+haniboy87@users.noreply.github.com>
2024-09-19 19:31:03 -0700

    Create pertemuan1_lovemeter_isvi hani ramadhan.py

---
+++ pertemuan1_lovemeter_isvi hani ramadhan.py
@@ -0,0 +1,15 @@
+impor acak
+
+print("SELAMAT DATANG DI LOVEMETER")
+
+nama_dia = input("masukkan nama dia: ")
+
+cocok = acak. acak()
+print("Kecocokan anda", cocok * 100, "%")
+
+jika cocok > 0,8:
+    print("anda sangat cocok dengan " + nama_dia + "!")
+elif 0.5 <= cocok <= 0.8:
+    print("anda lumayan cocok dengan " + nama_dia + "!")
+lainnya:
+    print("anda tidak cocok dengan " + nama_dia + "!")
