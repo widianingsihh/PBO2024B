22731fc8b0928abe85859d62433de862fd910597
haniboy87 <154058137+haniboy87@users.noreply.github.com>
2024-09-19 19:56:18 -0700

    Add files via upload

--- pertemuan1_lovemeter_isvi hani ramadhan.py
+++ pertemuan1_lovemeter_isvi hani ramadhan.py
@@ -1,15 +1,15 @@
-impor acak
+import random
 
 print("SELAMAT DATANG DI LOVEMETER")
 
-nama_dia = input("masukkan nama dia: ")
+nama_dia = input("masukkan nama dia")
 
-cocok = acak. acak()
+cocok = random.random()
 print("Kecocokan anda", cocok * 100, "%")
 
-jika cocok > 0,8:
+if cocok > 0.8:
     print("anda sangat cocok dengan " + nama_dia + "!")
 elif 0.5 <= cocok <= 0.8:
     print("anda lumayan cocok dengan " + nama_dia + "!")
-lainnya:
+else:
     print("anda tidak cocok dengan " + nama_dia + "!")
--- taktik_isvi hani ramadhan_challenge.py
+++ taktik_isvi hani ramadhan_challenge.py
@@ -1,15 +1,15 @@
-def hitung_dps(daya tembak, rate_of_fire):
-    dps = (daya tembak * rate_of_fire) / 60
-    Putaran Pengembalian (DPS, 2) 
+def hitung_dps(firepower, rate_of_fire):
+    dps = (firepower * rate_of_fire) / 60
+    return round(dps, 2) 
 
 
-def combat_effectiveness (daya tembak, rate_of_fire, akurasi, penghindaran):
+def combat_effectiveness(firepower, rate_of_fire, accuracy, evasion):
     combat_effectiveness = (
-        30 * daya tembak + 
+        30 * firepower + 
         40 * (rate_of_fire**2 / 120) +
-        15 * (akurasi + penghindaran)
+        15 * (accuracy + evasion)
     )
-    kembalikan int(combat_effectiveness)
+    return int(combat_effectiveness)
 
 
 print("Masukkan informasi Tactical Doll milikmu:")
@@ -21,7 +21,7 @@ evasion = int(input("Masukkan nilai Evasion: "))
 
 
 
-print("\n Masukkaninformasi Tactical Doll musuh:")
+print("\nMasukkan informasi Tactical Doll musuh:")
 nama_tactical_doll_musuh = input("Masukkan nama Tactical Doll: ")
 firepower_musuh = int(input("Masukkan nilai Firepower: "))
 rate_of_fire_musuh = int(input("Masukkan nilai Rate of Fire: "))
@@ -29,39 +29,40 @@ accuracy_musuh = int(input("Masukkan nilai Accuracy: "))
 evasion_musuh = int(input("Masukkan nilai Evasion: "))
 
 
-dps = hitung_dps(daya tembak, rate_of_fire)
-CE = combat_effectiveness (Daya Tembak, rate_of_fire, Akurasi, Penghindaran)
+dps = hitung_dps(firepower, rate_of_fire)
+ce = combat_effectiveness(firepower, rate_of_fire, accuracy, evasion)
 
 
-dps_milikmu = hitung_dps (daya tembak, rate_of_fire)
-ce_milikmu = combat_effectiveness (daya tembak, rate_of_fire, akurasi, penghindaran)
+dps_milikmu = hitung_dps(firepower, rate_of_fire)
+ce_milikmu = combat_effectiveness(firepower, rate_of_fire, accuracy, evasion)
 
 
 dps_musuh = hitung_dps(firepower_musuh, rate_of_fire_musuh)
 ce_musuh = combat_effectiveness(firepower_musuh, rate_of_fire_musuh, accuracy_musuh, evasion_musuh)
 
 
-jika ce_milikmu > ce_musuh:
+if ce_milikmu > ce_musuh:
     langkah = "MAJUU!!"
-lainnya:
-    langkah = "KABUR"🏃
+else:
+    langkah = "KABUR🏃"
 
 
 nama_lengkap = input("\nMasukkan Nama Lengkap Anda: ")
 nim = input("Masukkan NIM Anda: ")
 
 
-print("\n===== Info Tantangan Boneka Taktis =====")
+print("\n===== Tactical Doll Challenge Info =====")
 print(f"Nama Lengkap Anda: {nama_lengkap}")
 print(f"NIM Anda: {nim}")
 
-print("\n===== Boneka Taktis Milikmu =====")
+print("\n===== Tactical Doll Milikmu =====")
 print(f"Nama Tactical Doll: {nama_tactical_doll}")
-print(f"Kerusakan per Detik (DPS): {dps_milikmu}")
-print(f"Efektivitas Tempur (CE): {ce_milikmu}")
+print(f"Damage per Second (DPS): {dps_milikmu}")
+print(f"Combat Effectiveness (CE): {ce_milikmu}")
 
-print("\n===== Boneka Taktis Musuh =====")
+print("\n===== Tactical Doll Musuh =====")
 print(f"Nama Tactical Doll: {nama_tactical_doll_musuh}")
-print(f"Kerusakan per Detik (DPS): {dps_musuh}")
-print(f"Efektivitas Tempur (CE): {ce_musuh}")
+print(f"Damage per Second (DPS): {dps_musuh}")
+print(f"Combat Effectiveness (CE): {ce_musuh}")
+
 print(f"\nLangkah yang harus diambil: {langkah}")
