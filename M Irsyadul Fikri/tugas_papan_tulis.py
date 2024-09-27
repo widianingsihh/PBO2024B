2e7e5740421c4593f5da720a4048789b1d33816b
Fikri-14 <fikrimirsyadul@gmail.com>
2024-09-27 09:01:11 +0700

    Add files via upload

---
+++ M Irsyadul Fikri/tugas_papan_tulis.py
@@ -0,0 +1,33 @@
+# ~             CF2                         ~ 
+# 1. Class And Object in The_init_("Function")
+
+class PBOB:
+    def __init__(deklarasi, nama, jenis_kelamin):
+        deklarasi.nama = nama
+        deklarasi.jenis_kelamin = jenis_kelamin
+
+CF = PBOB("M.Irsyadul fikri", "Laki-Laki")
+
+print (CF.nama)
+print (CF.jenis_kelamin)
+
+print("*OR*")
+
+print ("Nama " + CF.nama)
+print ("Jenis Kelamin " + CF.jenis_kelamin)
+
+# 2.Alternate
+class mahasiswa :
+    def __init__(deklarasi, nama, jenis_kelamin, mahasiswa):
+        deklarasi.nama = nama
+        deklarasi.jenis_kelamin = jenis_kelamin
+        deklarasi.mahasiswa = mahasiswa
+
+    def info(deklarasi):
+        return f"Nama saya adalah {deklarasi.nama} dan Jenis Kelamin saya {deklarasi.jenis_kelamin} status Mahasiswa {deklarasi.mahasiswa}"
+
+mahasiswa1 = mahasiswa("M.Irsyadul fikri", "Laki-Laki", "PBO Kelas B")
+mahasiswa2 = mahasiswa("Hestifani Lesya", "Perempuan", "PBO Kelas A")
+
+print (mahasiswa1.info())
+print (mahasiswa2.info())
